b1bc05e40733c0b473608d3084ce40f21e2c1c8e
hekang42 <hekang@42seoul.student.kr>
2021-08-11 13:53:53 +0900

    sk

--- src/services/post.js
+++ src/services/post.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-const serviceKey = process.env.SERVICE_KEY;
-serviceKey = "EwASIYoegy8BFcuqV1HMC2T7Gk3f5OldW";
+const serviceKey = "EwASIYoegy8BFcuqV1HMC2T7Gk3f5OldW";
 
 export function getPost() {
 	return axios.get(`https://api.corona-19.kr/korea/?serviceKey=` + serviceKey);
