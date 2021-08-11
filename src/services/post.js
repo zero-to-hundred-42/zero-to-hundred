ca0036a0666f806440a99f3b4745eef28b382f28
hekang42 <hekang@42seoul.student.kr>
2021-08-11 12:26:24 +0900

    colsole_test

--- src/services/post.js
+++ src/services/post.js
@@ -4,6 +4,7 @@ const serviceKey = process.env.SERVICE_KEY;
 
 export function getPost() {
 	console.log(process.env.SERVICE_KEY);
+	console.log(process.env.SK);
 	return axios.get(`https://api.corona-19.kr/korea/?serviceKey=` + serviceKey);
 }
 
