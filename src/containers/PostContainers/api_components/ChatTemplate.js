9dd3f328971a88dc0fd56440f3a48b07a3f3bb4c
Eomhyunjun <zzp1318@gmail.com>
2021-08-14 15:39:00 +0900

    no_api just json

--- src/containers/PostContainers/api_components/ChatTemplate.js
+++ src/containers/PostContainers/api_components/ChatTemplate.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Get_API  from "./Get_API"
+import ex_data from "../db/data.json";
 
 /*
 ** ChatTemplate의 return 에서 MsgContent 태그에 들어갈 내용들 담는 컨포넌트 입니다.
@@ -25,8 +25,7 @@ function MsgContent({ data }) {
 
 
 export default function ChatTemplate() {
-
-	const [loca_cont, setLocacont] = useState(<tr><td>안녕하세요</td></tr>)
+	const [loca_cont, setLocacont] = useState(see_all_list());
 	const [data, setData] = useState([
 		{
 			"create_date": "",
@@ -40,19 +39,57 @@ export default function ChatTemplate() {
 	]);
 
 	/*
-	** Get_API 함수는 API정보를 땡겨서 db의 data.json에 정보를 넣어줌니다. 15초 단위로 setInterval 합니다
-	*/
+	location sort 해보려고 하는데 미완 하고 다시 푸쉬할게요
+var ret = ex_data.data.map( (data) => {
+	const res = {"num": data.location_id, "name" : data.location_name}
+	return res;
+});
 
+const arr_ret = ret.sort(function(a, b) {
+	return a.num - b.num;
+})
+
+const set = new Set(arr_ret);
+const uniqueArr = [...set];
+
+console.log("ret :");
+console.log(uniqueArr);
+
+*/
+
+	/*
+	Get_API 함수는 API정보를 땡겨서 db의 data.json에 정보를 넣어줌니다. 15초 단위로 setInterval 합니다
 	let num = 1;
 	setInterval(()=> {
 		Get_API(num);
 		num += 1;
 	},1500);
-
+	*/
+	
 	
 	/*
 	** tbody 안의 {loca_cont} 에 들어갈 태그들을 담는 함수입니다.
 	*/
+	function see_all_list() {
+
+		const onClick = (e) => {
+			const result = ex_data.data.filter((data) => data.location_name === e.currentTarget.value);
+			setData(result);
+		}
+
+		const result = ex_data.location.map(
+			(value) => {
+				return (<tr key={value.num}>
+					<td>
+						<button onClick={onClick} value={value.name}>
+							{value.name}
+						</button>
+					</td>
+				</tr>)
+		});
+		return result
+	}
+
 	function see_list(data) {
 		console.log("this_is_see_list");
 		console.log(data);
@@ -66,9 +103,29 @@ export default function ChatTemplate() {
 					</td>
 				</tr>)
 		});
-		console.log(result);
 		setLocacont(result);
 	}
+
+
+	/*
+	** input 태그에 값을 검색 후 나오는 버튼을 클릭했을 때
+	** API에서 해당 값을 찾아 가져와주는 클릭 이벤트 함수입니다.
+	*/
+
+	const onClick = (e) => {
+		/* 
+		fetch(`http://localhost:3001/data?location_name=${e.currentTarget.value}`)
+		.then((first) => first.json())
+		.then((last) => {
+			console.log("test");
+			console.log(last);
+			setData(last);
+		});
+		*/
+		const result = ex_data.data.filter((data) => data.location_name === e.currentTarget.value);
+		setData(result);
+	}
+
 
 	/*
 	** <input type="text" onChange={find_Location} /> 태그에 입력 되는 값들을 가져와서
@@ -77,12 +134,16 @@ export default function ChatTemplate() {
 
 	function find_Location(e) {
 		var search_this = e.target.value;
+		/*
+		** rest-api 사용 시 마지막 then에 값을 넣어주면 됩니다.
 		fetch(`http://localhost:3001/location`)
 		.then((res) => {
 			if (res.ok) return res.json();
 		})
-		.then((data) => {
-			const result = data.filter((odata) => {
+		.then((data) => { });
+		*/
+
+			const result = ex_data.location.filter((odata) => {
 				if (odata.name.indexOf(search_this) > -1)
 					return odata.name;
 				return 0;
@@ -92,7 +153,7 @@ export default function ChatTemplate() {
 			if (e.target.value === "")
 			{
 				console.log("내용 없음");
-				setLocacont(<tr><td>안녕하세요</td></tr>);
+				see_list(ex_data.location);
 			}
 			else if (result === undefined)
 			{
@@ -104,24 +165,8 @@ export default function ChatTemplate() {
 				console.log("내용 있음");
 				see_list(result);
 			}
-		});
+
 	}
-
-
-	/*
-	** input 태그에 값을 검색 후 나오는 버튼을 클릭했을 때
-	** API에서 해당 값을 찾아 가져와주는 클릭 이벤트 함수입니다.
-	*/
-
-	const onClick = (e) => {
-		fetch(`http://localhost:3001/data?location_name=${e.currentTarget.value}`)
-		.then((first) => first.json())
-		.then((last) => {
-			console.log("test");
-			console.log(last);
-			setData(last);
-		});
-	};
 
 	return (
 		<div
@@ -134,7 +179,7 @@ export default function ChatTemplate() {
 					<thead>
 						<tr>
 							<th>
-								<input type="text" onChange={find_Location} />
+								<input type="text" onChange={find_Location} value="" />
 							</th>
 						</tr>
 					</thead>
@@ -152,3 +197,4 @@ export default function ChatTemplate() {
 		</div>
 	);
 }
+
